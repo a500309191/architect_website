81c45d5caf09ff1dee66dc05c24749ace92804c4
a500309191 <a500309191@gmail.com>
2022-02-24 00:03:07 +0300

    2_240222

--- arch/models.py
+++ arch/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+"""
+HOUSE ATTRIBUTES:
+Model 
+Area
+Floors - [1,2,3,4,5]
+Size - [tiny, medium, large]
+Dimensions - [n*m]
+Material - [stone, wood]
+Roof - [pitched, flat]
+Style - [traditional, modern]
+Rooms - [kitchen, bedroom, washroom, ...]
+Images
+"""
+
+class House(models.Model):
+    model = models.CharField(max_length=50, verbose_name="model number")
+    area = models.FloatField(default=0.0)
+
+    floors_choice = ((1,'1'),
+                     (2,'2'),
+                     (3,'3'),
+                     (4,'4'))
+    floors = models.IntegerField(choices=floors_choice, default=1)
+
+    def __str__(self):
+        return self.model
+
+
+class Image(models.Model):
+    house = models.ForeignKey('House',
+                                on_delete=models.CASCADE,
+                                help_text="Please attach images",
+                                verbose_name="house_id",
+                                null=True,
+                                blank=False)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
+
+    def __str__(self):
+        return '%s' % self.image
