63738582eb7c22cec06a73d5ee0e12e03cd13956
a500309191 <a500309191@gmail.com>
2022-03-15 23:28:23 +0300

    I dont know what should I text here... I did describe almost all house properties. Next time I need to figure out how to describe Rooms and Dimentions models

--- arch/admin.py
+++ arch/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from . models import House, Image
+from . models import House, Image, Material, Style
 
 admin.site.register(House)
 admin.site.register(Image)
+admin.site.register(Material)
+admin.site.register(Style)
--- arch/models.py
+++ arch/models.py
@@ -6,27 +6,54 @@ Model
 Area
 Floors - [1,2,3,4,5]
 Size - [tiny, medium, large]
-Dimensions - [n*m]
 Material - [stone, wood]
 Roof - [pitched, flat]
 Style - [traditional, modern]
 Rooms - [kitchen, bedroom, washroom, ...]
+Dimensions - [n*m]
 Images
 """
 
 class House(models.Model):
-    model = models.CharField(max_length=50, verbose_name="model number")
-    area = models.FloatField(default=0.0)
+    model = models.CharField(max_length=50,
+                             verbose_name="model number")
+    area = models.FloatField(default=0.00)
 
     floors_choice = ((1,'1'),
                      (2,'2'),
                      (3,'3'),
                      (4,'4'))
-    floors = models.IntegerField(choices=floors_choice, default=1)
+    floors = models.IntegerField(choices=floors_choice,
+                                 default=1)
+
+    sizes_choice = ((1, 'tiny'),
+                    (2, 'medium'),
+                    (3, 'large'))
+    size = models.IntegerField(choices=sizes_choice,
+                               help_text="Choose house size",
+                               verbose_name="House size")
+
+    material = models.ForeignKey('Material',
+                                 on_delete=models.CASCADE,
+                                 help_text="Choose material type",
+                                 verbose_name="Material type",
+                                 null=True)
+
+    style = models.ForeignKey('Style',
+                              on_delete=models.CASCADE,
+                              help_text="Choose architectural style",
+                              verbose_name="Architectural style",
+                              null=True)
+
+    roofs_choice = ((1,'pitched'),
+                    (2,'flat'))
+    roof = models.IntegerField(choices=roofs_choice,
+                               help_text="Choose roof type",
+                               verbose_name="Roof type",
+                               null=True)
 
     def __str__(self):
         return self.model
-
 
 class Image(models.Model):
     house = models.ForeignKey('House',
@@ -39,3 +66,20 @@ class Image(models.Model):
 
     def __str__(self):
         return '%s' % self.image
+
+class Material(models.Model):
+    name = models.CharField(max_length=30,
+                            help_text="Input material type",
+                            verbose_name="Material type")
+
+    def __str__(self):
+        return self.name
+
+class Style(models.Model):
+    name = models.CharField(max_length=30,
+                            help_text="Input architectural style",
+                            verbose_name="Architectural style")
+
+    def __str__(self):
+        return self.name
+
